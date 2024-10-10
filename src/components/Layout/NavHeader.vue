47ebff1f1acfb1ea18307b09616ff3800e4b46e3
sammy-code98 <sammysmart98@yahoo.com>
2024-10-10 17:35:49 +0100

    feat:project scalfold - layout setup - code clean up

---
+++ src/components/Layout/NavHeader.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-app-bar class="py-md-1" :elevation="0">
+    <v-toolbar-title class="text-h5 font-weight-bold">Reports</v-toolbar-title>
+    <v-spacer />
+    <v-btn class="text-capitalize" prepend-icon="mdi-download" variant="plain">
+      Download
+    </v-btn>
+  </v-app-bar>
+  <v-divider />
+</template>
+
+<script>
+  import { defineComponent } from 'vue'
+
+  export default defineComponent({
+    name: 'NavHeader',
+  })
+</script>
