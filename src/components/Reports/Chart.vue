e268d9634232fd1fe754f5ddae505e3e69a37d10
sammy-code98 <sammysmart98@yahoo.com>
2024-10-11 10:48:53 +0100

    implement chart ui

--- src/components/Reports/Chart.vue
+++ src/components/Reports/Chart.vue
@@ -5,7 +5,9 @@
         <v-col cols="6">
           <v-row class="flex-wrap">
             <v-col v-for="i in 6" :key="i" class="d-flex justify-center align-center" cols="4">
-              <div class="pa-md-4 bg-white rounded-xl border" style="width: 100%; height: 152.4px;">lol</div>
+              <div style="height: 100%; width: 100%;">
+                <div class="pa-md-4 bg-white rounded-xl border" style="width: 100%; height: 152.4px;">lol</div>
+              </div>
             </v-col>
           </v-row>
         </v-col>
@@ -23,7 +25,11 @@
               />
             </div>
             <v-divider class="mt-n4" />
+            <div class="py-1">
+              <apexchart height="350" :options="chartOptions" :series="series" type="bar" />
+            </div>
           </div>
+
         </v-col>
       </v-row>
     </v-container>
@@ -32,17 +38,69 @@
 
 <script lang="ts">
   import { defineComponent, ref } from 'vue'
+  import VueApexCharts from 'vue3-apexcharts'
 
   export default defineComponent({
     name: 'Chart',
+    components: {
+      apexchart: VueApexCharts,
+    },
     setup () {
       type ActivityI = string[]
       const selectedActivityPeriod = ref('Month')
 
       const activity:ActivityI = ['Week', 'Month', 'Year']
+
+      const chartOptions = ref({
+        chart: {
+          type: 'bar',
+          height: 350,
+        },
+        plotOptions: {
+          bar: {
+            borderRadius: 5,
+            columnWidth: '24%',
+          },
+        },
+        dataLabels: {
+          enabled: false,
+        },
+        xaxis: {
+          categories: ['JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC'],
+        },
+      })
+      const series = ref([
+        {
+          name: 'Activity',
+          data: [100, 150, 200, 240, 300, 101, 350, 320, 125, 333, 380, 400],
+          fill: {
+            type: 'linear-gradient',
+            x1: 0,
+            y1: 0,
+            x2: 1,
+            y2: 1,
+            colorStops: [
+              {
+                offset: 0,
+                color: 'rgba(27, 89, 248, 0.8)',
+                opacity: 1,
+
+              },
+              {
+                offset: 1,
+                color: 'rgba(27, 89, 248, 0)',
+                opacity: 1,
+              },
+            ],
+          },
+        },
+      ])
+
       return {
         activity,
         selectedActivityPeriod,
+        chartOptions,
+        series,
       }
     },
   })
